cb598882a0d939cddb3d1187e085f973a5ac47af
schoolmanagement2030-cloud <schoolmanagement2030@gmail.com>
2026-03-11 06:53:52 +0530

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,23 @@
+function principal(){
+
+alert("Principal Login Page");
+
+}
+
+function teacher(){
+
+alert("Teacher Login Page");
+
+}
+
+function driver(){
+
+alert("Driver Login Page");
+
+}
+
+function parent(){
+
+alert("Parent Login Page");
+
+}
