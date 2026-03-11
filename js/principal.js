eeb754f6e66013cdd5296104df6cac81b611454d
schoolmanagement2030-cloud <schoolmanagement2030@gmail.com>
2026-03-11 14:11:03 +0530

    Update principal.js

--- js/principal.js
+++ js/principal.js
@@ -1,23 +1,35 @@
 function openStudents(){
+
 alert("Students Management");
+
 }
 
 function openTeachers(){
+
 alert("Teachers Management");
+
 }
 
 function openBus(){
-alert("Bus Tracking System");
+
+window.location.href="bus-map.html";
+
 }
 
 function openExpenses(){
+
 alert("Expense Manager");
+
 }
 
 function openMessages(){
+
 alert("Message Center");
+
 }
 
 function logout(){
-window.location.href = "index.html";
+
+window.location.href="index.html";
+
 }
