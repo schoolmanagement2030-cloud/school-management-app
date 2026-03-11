15b65fc4e257acf50de923e6792359f81e53acbd
schoolmanagement2030-cloud <schoolmanagement2030@gmail.com>
2026-03-11 07:27:15 +0530

    Create principal.js

---
+++ js/principal.js
@@ -0,0 +1,23 @@
+function openStudents(){
+alert("Students Management");
+}
+
+function openTeachers(){
+alert("Teachers Management");
+}
+
+function openBus(){
+alert("Bus Tracking System");
+}
+
+function openExpenses(){
+alert("Expense Manager");
+}
+
+function openMessages(){
+alert("Message Center");
+}
+
+function logout(){
+window.location.href = "index.html";
+}
