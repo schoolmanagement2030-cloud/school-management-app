e86b81735871b630fc88fc675c4294d314ce841d
schoolmanagement2030-cloud <schoolmanagement2030@gmail.com>
2026-03-12 20:02:17 +0530

    Add Firebase Storage service to firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,7 +1,12 @@
 // Import Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
 
 // Firebase Config
 const firebaseConfig = {
@@ -13,12 +18,16 @@ const firebaseConfig = {
   appId: "1:698409232180:web:schoolsphere"
 };
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 
 // Services
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
+
 
 // Export
-export { auth, db };
+export { auth, db, storage };
