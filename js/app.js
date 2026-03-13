787c1f05d04fd6fac54f3add3baffe316b26e40e
schoolmanagement2030-cloud <schoolmanagement2030@gmail.com>
2026-03-13 05:43:51 +0530

    Add logout function to redirect and alert user

--- js/app.js
+++ js/app.js
@@ -15,3 +15,15 @@ window.location.href = "../html/driver.html";
 function parent(){
 window.location.href = "../html/parent.html";
 }
+
+
+// Logout Function
+function logout(){
+
+// Firebase logout later add होगा
+alert("Logged Out");
+
+// Login page
+window.location.href = "../html/index.html";
+
+}
